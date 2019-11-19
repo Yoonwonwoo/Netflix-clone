169e0ef3fdb923344db183ab150b84543d22d075
Yoonwonwoo <yooonwonwoo0303@gmail.com>
2019-11-19 16:43:11 +0900

    숙제 끝

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,13 +6,14 @@ width: 100vw;
 height: 60px;
 display: flex;
 flex-direction: row;
-padding: 13px 58px;
+align-items: center;
+padding: 2px 40px;
 box-sizing: border-box;
 `;
 
 const Logo = styled.img`
-height: 100%;
-margin: 10px 0;
+width: 70px;
+margin-right: 23px;
 `;
 
 const NavText = styled.ul`
@@ -22,36 +23,54 @@ color: white;
 height: 100%;
 flex: 1;
 align-items: center;
-margin: 10px 0;
+margin: 8px 0;
+font-size: 10px;
 `;
 
 const NavTextItem = styled.li`
+margin-left: 14px;
+
+&:hover {
+    color : #b3b3b3;
+    font-weight: normal;
+}
 `;
 
 const NavIcon = styled.ul`
 display: flex;
+align-items: center;
 list-style: none;
 height: 100%;
+margin: 16px 0;
 `;
 
 const NavIconItem = styled.li`
 width: fit-content;
+padding: 10px;
 
 & > img {
-    height: 100%;
+    height: 16px;
 }
 `;
 
-const Banner = props => {
+
+const Banner = () => {
+
+    const [num, setNum] = React.useState(0);
+
+    const change = (e) => {
+        setNum(e);
+    }
+
     return (
         <Wrapper>
         <Logo src="https://cdn.pixelprivacy.com/wp-content/uploads/2017/12/Netflix-Logo-1024x277.png"/>
         <NavText>
-            <NavTextItem>홈</NavTextItem>
-            <NavTextItem>TV프로그램</NavTextItem>
-            <NavTextItem>영화</NavTextItem>
-            <NavTextItem>최신 등록 콘텐츠</NavTextItem>
-            <NavTextItem>내가 찜한 콘텐츠</NavTextItem>
+            <NavTextItem onClick={e => change(0)} style={num === 0 ? {color: "#000fff", fontWeight: "bold"} : {}}>홈</NavTextItem>
+            <NavTextItem onClick={e => change(1)} style={num === 1 ? {color: "#000fff", fontWeight: "bold" } : {}}>TV프로그램</NavTextItem>
+            <NavTextItem onClick={e => change(2)} style={num === 2 ? {color: "#000fff", fontWeight: "bold" } : {}}>영화</NavTextItem>
+            <NavTextItem onClick={e => change(3)} style={num === 3 ? {color: "#000fff", fontWeight: "bold" } : {}}>최신 등록 콘텐츠</NavTextItem>
+            <NavTextItem onClick={e => change(4)} style={num === 4 ? {color: "#000fff", fontWeight: "bold" } : {}}>내가 찜한 콘텐츠</NavTextItem>
         </NavText>
         <NavIcon>
             <NavIconItem><img src="/search.svg" alt=""/></NavIconItem>
